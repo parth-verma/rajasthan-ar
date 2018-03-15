f879644f401c72dba53fde92480bcfe0206e9a65
Parth Verma <v.parth98@gmail.com>
2018-03-15 22:52:58 +0530

    Initial Commit

---
+++ Assets/hideButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviours : MonoBehaviour {
+
+	void changeCanvasProperty(GameObject yourCanvas, bool disable) {
+		if (!disable)
+			yourCanvas.SetActive(false);
+		else
+			yourCanvas.SetActive(true);
+	}
+}
